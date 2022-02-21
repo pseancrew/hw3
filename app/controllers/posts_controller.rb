e2a5abaced081f9a150f4ad88024c27b43b3ffc5
pseancrew <sean.paquet@kellogg.northwestern.edu>
2022-02-21 15:07:17 +0000

    updates post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,13 +5,14 @@ class PostsController < ApplicationController
 
     def new
         @post = Post.new 
-        @post.place_id = params["place_id"]
+        @place = Place.find(params["place_id"])
+        @post.place_id = @place.id
     end
     
     def create
         @post = Post.new(params["post"])
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places/#{@post.place.id}"
     end
 
 end
